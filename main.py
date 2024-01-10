748520b92a97e3b045fb2d7f53cb5ca86570ade0
Ben Link <link.ben@gmail.com>
2024-01-10 13:30:37 -0500

    set up boilerplate

---
+++ main.py
@@ -0,0 +1,12 @@
+from config.constants import *
+
+class CTTS:
+    def __init__(self):
+        pass
+
+    def run():
+        pass
+
+if __name__ == "__main__":
+    game = CTTS()
+    game.run()
